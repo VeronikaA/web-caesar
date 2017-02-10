e054619c9cb996d0bec419e52ed32a9f165cbbf7
Perigree <holisticspiral@gmail.com>
2017-02-10 11:47:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,10 +24,11 @@ def build_page( textarea_content):
     rotation_input= "<input type= 'number' name = 'rotation'/>"
 
     message_label = "<label>Type a Message:</label>"
-    textarea = "<textarea name = 'message'  + textarea_content + "</textarea>">
+    textarea = "<textarea name = 'message'  + textarea_content + "</textarea><br>" >
     submit = "<input type = 'submit'/>"
-    form = ("<form method= 'post'>" +  rot_label + rotation_input + "<br>" +
-    message_label + text_area +"<br>"+ "<br>"
+    form = ("<form method = 'post'>" +
+                rot_label + rotation_input + "<br>" +
+                message_label + textarea + "<br>" + submit + "</form>")
     + submit + "</form>")
 
     
